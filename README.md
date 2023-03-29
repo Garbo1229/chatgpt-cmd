5c1f588be3565fac26b0c0fbe084dd2261c61520
Garbo <i@garbo.me>
2023-03-29 18:29:52 +0800

    feat:支持连续问答

--- README.md
+++ README.md
@@ -3,17 +3,21 @@
 使用步骤：
 
 1. 复制代码
-git clone https://github.com/Garbo1229/chatgpt-cmd.git
+    `git clone https://github.com/Garbo1229/chatgpt-cmd.git`
 
-2. 修改apiKey的变量的值
+2. 修改 `main.go` 中`apiKey` 的变量的值
+    ```
+    ...
+    // 替换为您的API密钥
+	var apiKey = ""
+    ...
+    ```
 
-3. 修改~/.bashrc 或 ~/.zshrc
+3. 修改 `~/.bashrc` 或 `~/.zshrc`
     ```
     // 最后一行添加 PATH 为你的路径
     ...
-    chatgpt (){
-    go run PATH/main.go -m {$1}
-    }
+    alias chatgpt="go run <PATH>/main.go"
     ```
 
 4. 立即生效
@@ -22,8 +26,6 @@ git clone https://github.com/Garbo1229/chatgpt-cmd.git
     ```
 
 5. 测试
-    ```
-    chatgpt "test"
-    ```
+    `chatgpt`
 
 Enjoy it ~
--- main.go
+++ main.go
@@ -1,52 +1,96 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"encoding/json"
-	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
+type result struct {
+	code int64
+	msg  string
+	data map[string]interface{}
+	err  error
+}
+
+// 替换为您的API密钥
+var apiKey = ""
+
+// ChatGPT API的URL
+var apiURL = "https://api.openai.com/v1/chat/completions"
+
 func main() {
-	// 提出的问题
-	var content string
 
-	//注册参数
-	flag.StringVar(&content, "m", "", "提出的问题,默认为空")
-
-	flag.Parse()
-
-	if content == "" {
-		fmt.Printf("请加入 -m 参数提问")
-		return
+	if apiKey == "" {
+		fmt.Println("请替换您的API秘钥后再执行")
+		os.Exit(0)
 	}
-	// 替换为您的API密钥
-	apiKey := ""
 
-	// ChatGPT API的URL
-	apiURL := "https://api.openai.com/v1/chat/completions"
+	closeHandler()
+
+	// 消息数组
+	messages := []interface{}{}
+
+	for true {
+		// 输入问题
+		var input string
+
+		fmt.Print("[Me]: ")
+
+		scanner := bufio.NewScanner(os.Stdin)
+		if scanner.Scan() {
+			input = scanner.Text()
+		}
+
+		// 消息列表
+		messages = append(messages, map[string]string{"role": "user", "content": input})
+
+		// 请求结果
+		result := requestHandler(messages)
+
+		if result.code == 200 {
+			// 成功
+			messages = append(messages, map[string]string{"role": "assistant", "content": result.msg})
+			fmt.Printf("[ChatGPT]: %v\n", result.msg)
+		} else if result.code == 500 {
+			// API错误
+			fmt.Printf("[ChatGPT Error]: %v\n[Response Data]: %v\n", result.msg, result.data)
+			break
+		} else if result.err != nil {
+			// 请求过程中错误
+			fmt.Printf("[System]: %v\n[Response Data]: %v\n", result.err, result.data)
+			break
+		}
+
+	}
+}
+
+// 构建请求
+func requestHandler(messages []interface{}) result {
 
 	// 构造请求的数据
 	requestData := map[string]interface{}{
 		"model":       "gpt-3.5-turbo",
-		"messages":    [...]interface{}{map[string]string{"role": "user", "content": content}},
+		"messages":    messages,
 		"temperature": 0.7,
 	}
 
 	// 将请求的数据编码为JSON格式
 	requestBody, err := json.Marshal(requestData)
 	if err != nil {
-		fmt.Println("Error encoding JSON:", err)
-		return
+		return result{err: err}
 	}
 
 	// 创建POST请求
 	request, err := http.NewRequest("POST", apiURL, bytes.NewBuffer(requestBody))
 	if err != nil {
-		fmt.Println("Error creating request:", err)
-		return
+		return result{err: err}
 	}
 
 	// 添加请求头信息
@@ -54,32 +98,29 @@ func main() {
 	request.Header.Set("Authorization", "Bearer "+apiKey)
 
 	// 发送请求
-	client := &http.Client{}
+	client := &http.Client{Timeout: 5000 * time.Second}
 	response, err := client.Do(request)
 	if err != nil {
-		fmt.Println("Error sending request:", err)
-		return
+		return result{err: err}
 	}
 
 	// 读取响应的数据
 	responseBody, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		fmt.Println("Error reading response:", err)
-		return
+		return result{err: err}
 	}
 
 	// 解码响应的数据
 	var responseData map[string]interface{}
 	err = json.Unmarshal(responseBody, &responseData)
 	if err != nil {
-		fmt.Println("Error decoding JSON:", err)
-		return
+		return result{err: err}
 	}
+
 	// 处理错误
 	if errorData, ok := responseData["error"]; ok {
 		if errorMessgae, ok := errorData.(map[string]interface{})["message"]; ok {
-			fmt.Printf("[Error Message]: %s \n", errorMessgae)
-			return
+			return result{code: 500, msg: errorMessgae.(string), data: requestData}
 		}
 
 	}
@@ -87,10 +128,20 @@ func main() {
 	if choices, ok := responseData["choices"]; ok {
 		if message, ok := choices.([]interface{}); ok {
 			if content, ok := message[0].(map[string]interface{}); ok {
-				fmt.Printf("[ChatGPT]: %v \n", content["message"].(map[string]interface{})["content"])
-				return
+				return result{code: 200, msg: content["message"].(map[string]interface{})["content"].(string)}
 			}
 		}
 	}
-	fmt.Printf("[System Error]: ResponData  --- %v --- \n", requestData)
+	return result{code: 500, msg: "出现未知错误", data: requestData}
+}
+
+// 处理关闭提示
+func closeHandler() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		<-c
+		fmt.Println("\rGood bye!~")
+		os.Exit(0)
+	}()
 }
